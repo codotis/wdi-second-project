34e0d46f56ab27f07d007798e85b8b9c7cf51bc8
Otis <otisvickersgraver@gmail.com>
2018-01-15 16:05:40 +0000

    can now edit tracks when logged in

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express        = require('express');
 const morgan         = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const session        = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
 const routes         = require('./config/routes');
@@ -21,6 +22,14 @@ app.use(express.static(`${__dirname}/public`));
 if(env === 'development') app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride((req) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.use(session({
   secret: secret,
